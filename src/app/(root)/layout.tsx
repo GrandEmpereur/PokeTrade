c22e7f13101bc6cf12cf7b98ac210ff6dde7e08f
Patrick Bartosik <patrickbartosik@Host-001.lan>
2025-03-31 19:03:24 +0200

    ✨ (auth): add authentication layout and pages
    Introduce new authentication layout and pages for login and register.
    This includes a new background image, form validation using zod, and
    integration with react-hook-form. This change aims to provide a
    structured and user-friendly authentication flow for users.

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SubRootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default SubRootLayout
